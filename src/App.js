64e80d9f4dec89086884492ca3a74f570005a443
tienho1 <hotientl1@gmail.com>
2024-03-31 16:25:11 +0700

    buoi4lan1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Modal } from 'bootstrap';
+import React from 'react';
+import Hook6 from './Components/Hook6';
+
+
+
+
+const App = () => {
+    return (
+        <div>
+            <Hook6 />
+        </div>
+    );
+}
+
+export default App;
